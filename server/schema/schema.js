c222eb80ecb3e7453718c9e3e072e8c536d70c43
hanselcarter <barahonahansel@hotmail.com>
2020-04-25 16:15:08 -0600

    before removing dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,60 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
 
 //dummy data
 
 const books = [
-  { name: "test", genre: "action", id: "1" },
-  { name: "test2", genre: "fantasy", id: "2" },
+  { name: "tes1", genre: "action", id: "1", authorId: "2" },
+  { name: "test2", genre: "fantasy", id: "2", authorId: "2" },
+  { name: "test3", genre: "fantasy", id: "3", authorId: "1" },
+  { name: "tes4", genre: "action", id: "4", authorId: "2" },
+  { name: "test5", genre: "fantasy", id: "5", authorId: "2" },
+  { name: "test6", genre: "fantasy", id: "6", authorId: "1" },
 ];
+
+const authors = [
+  { name: "author1", age: 34, id: "1" },
+  { name: "author2", age: 35, id: "2" },
+];
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        const booksFiltered = books.filter(
+          (book) => book.authorId == parent.id
+        );
+        return booksFiltered;
+      },
+    },
+  }),
+});
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        const author = authors.find((author) => author.id == parent.authorId);
+        return author;
+      },
+    },
   }),
 });
 
@@ -22,11 +63,32 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db
         const book = books.find((book) => book.id == args.id);
         return book;
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from db
+        const author = authors.find((author) => author.id == args.id);
+        return author;
+      },
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve() {
+        return books;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve() {
+        return authors;
       },
     },
   },
