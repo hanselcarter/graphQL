9ca679001f14b9faea4cadb47259df572c596017
hanselcarter <barahonahansel@hotmail.com>
2020-04-26 23:18:24 -0600

    added addbook mutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { useQuery } from "@apollo/react-hooks";
-import { getAuthorsQuery } from "../queries/queries";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { getAuthorsQuery, addBookMutation } from "../queries/queries";
 
 const AddBook = () => {
   const { loading, data } = useQuery(getAuthorsQuery);
+  const [addBook, { data: bookAddedData }] = useMutation(addBookMutation);
+  const [bookName, setBookName] = React.useState("");
+  const [bookGenre, setBookGenre] = React.useState("");
+  const [selectedAuthorId, setSelectedAuthorId] = React.useState("");
+
+  const onChangeBookName = (e) => {
+    e.preventDefault();
+    setBookName(e.target.value);
+  };
+
+  const onChangeBookGenre = (e) => {
+    e.preventDefault();
+    setBookGenre(e.target.value);
+  };
+
+  const onChangeAuthor = (e) => {
+    e.preventDefault();
+    setSelectedAuthorId(e.target.value);
+  };
+
+  const onClickAddBook = (e) => {
+    e.preventDefault();
+    if (bookName && bookGenre && selectedAuthorId) {
+      addBook({
+        variables: {
+          name: bookName,
+          genre: bookGenre,
+          authorId: selectedAuthorId,
+        },
+      });
+    }
+  };
 
   if (loading) {
     return <div>...loading</div>;
@@ -13,15 +45,15 @@ const AddBook = () => {
     <form>
       <div className="field">
         <label>Book Name</label>
-        <input type="text" />
+        <input type="text" onChange={onChangeBookName} />
       </div>
       <div className="field">
         <label>Genre</label>
-        <input type="text" />
+        <input type="text" onChange={onChangeBookGenre} />
       </div>
       <div className="field">
         <label>Author</label>
-        <select>
+        <select onChange={onChangeAuthor}>
           <option key="no valid" value={null}>
             Select author
           </option>
@@ -32,7 +64,7 @@ const AddBook = () => {
           ))}
         </select>
       </div>
-      <button>+</button>
+      <button onClick={onClickAddBook}>+</button>
     </form>
   );
 };
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -17,3 +17,11 @@ export const getBooksQuery = gql`
     }
   }
 `;
+
+export const addBookMutation = gql`
+  mutation addBook($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
+      id
+    }
+  }
+`;
